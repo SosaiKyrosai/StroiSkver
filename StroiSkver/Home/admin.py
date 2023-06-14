618e3278ecb10fd232fe49e33b0fe7c9851033ee
SosaiKyrosai <sosaikurosai@gmail.com>
2023-06-14 12:39:23 +0700

    Добавил модель

--- StroiSkver/Home/admin.py
+++ StroiSkver/Home/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import paper
 
-# Register your models here.
+admin.site.register(paper)
--- StroiSkver/Home/models.py
+++ StroiSkver/Home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class paper(models.Model):
+    title = models.CharField('Название', max_length=100)
+    textOfPaper = models.TextField('ТекстСтатьи')
+    pictureOfPaper = models.ImageField(blank=True, upload_to='images/')
+
+
+
+    def __str__(self):
+        return str(self.title)
