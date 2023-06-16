1027e1122e8de07b21dd5b8122c7fe840eb1e519
SosaiKyrosai <sosaikurosai@gmail.com>
2023-06-16 21:34:53 +0700

    Готова главная и база+ создано приложение товаров

--- StroiSkver/Home/views.py
+++ StroiSkver/Home/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import paper
 
 
 # Create your views here.
 def index (request):
-    return render(request, 'Home/Home.html')
+    papers = paper.objects.order_by('-id')  # тут_потом_сделать_минус_перед_айди
+    return render(request,'Home/Home.html',{'title': 'Главная', 'papers': papers})
