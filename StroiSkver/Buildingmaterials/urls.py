9e3ec222f723d88a64081320d93c27bf9325a179
SosaiKyrosai <sosaikurosai@gmail.com>
2023-06-23 17:34:22 +0700

    Добавлена фильтрация

--- StroiSkver/Buildingmaterials/urls.py
+++ StroiSkver/Buildingmaterials/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
 
     path('pagematerials/all/', views.product_list, name='materials'),
     path('pagematerials/<slug:category_slug>/', views.category_product_list, name='category_product_list'),
+    path('product/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('pagematerials/all/filter/', views.all_products_filter, name='all_products_filter'),
+    path('pagematerials/<slug:category_slug>/filter/', views.category_products_filter, name='category_products_filter'),
 
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
